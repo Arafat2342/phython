6190027983e9498748ef6a3cb31ccaaad9e0db62
Arafat2342 <jashoresinger@gmail.com>
2025-02-20 22:33:35 +0600

    py

--- data.py
+++ data.py
@@ -1,6 +1,10 @@
 age = 13;
 time = 2;
 
+#Story of my learning how to code
+
 print('My name is: Yasir')
 print("I'm learning coding since",time,'years.')
 print("Now my age is:",age+time)
+
+#this is my first python code
